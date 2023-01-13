e7e7babe962b16120c4776213854dabe21e44c47
S-Sneha <098sneha@gmail.com>
2023-01-13 22:52:24 +0530

    Adding the routes for book-record-mngt api

--- README.md
+++ README.md
@@ -1,8 +1,31 @@
 # book-record-management API
 
 # /users
- 
+ /users (done)
+POST: Create a new user GET: Get all list of users
+
+/users/{id} (done)
+GET: Get a user by thier ID PUT: Update a user by ID DELETE: Delete a user by thier ID (Check if the user still has an issued book && is there any fine to be collected from the user)
+
+/users/subscription-details/{id}
+GET: Get user subscription details
+
+Date of Subscription
+Valid till ??
+Fine if any ??
+
 # /books
+/books
+GET: Get all books POST: Add a new book
+
+/books/{id}
+GET: Get a book by id PUT: Update a book by Id
+
+/books/issued
+GET: Get all issued books here
+
+/books/issued/withFine
+GET: Get all issued books with fine
 
 # subscription type
 
--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 const express= require("express");
+
+//import db connection file
+const DbConnection = require("./databaseConnection");
+// import database
+const dotenv = require("dotenv");
+
+// import routes
+const usersRouter = require("./routes/users");
+const booksRouter = require("./routes/books");
+
+dotenv.config();
+
 const app = express();
 const PORT = 8081;
 
+DbConnection();
+
 app.use(express.json());
 
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+      message: "Server is up and running succesfully",
+    });
+  });
+  
+  app.use("/users", usersRouter);
+  app.use("/books", booksRouter);
+  
+  app.get("*", (req, res) => {
+    res.status(404).json({
+      message: "This route doesn't exist",
+    });
+  });
+  app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
+  });
